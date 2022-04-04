cb0f0bc5517da8d19b00980046557b1205a8abde
Nadine Siebrands <nsiebrands@ics-group.eu>
2022-04-04 12:52:49 +0200

    Projektdateien hinzufügen.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EMA
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            Age = 12;
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { OnPropertyChange(); _name = value; }
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set 
+            { 
+                _age = value; 
+                OnPropertyChange();
+            }
+        }
+
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EMA
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChange([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
